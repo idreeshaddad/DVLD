eb0248ce004bc8f52ab1ff0f994a428102cc6c70
Idrees Haddad <gelnox@live.com>
2021-03-08 17:50:43 +0200

    Added .Razor Runtime Compilation

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,8 @@ namespace DVLD
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
+
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
