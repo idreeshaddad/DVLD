67c2056982214c31e43f368acfe43c38f68c6375
Idrees Haddad <gelnox@live.com>
2021-03-17 17:44:31 +0200

    Added DataAnnotations (Attribute) to entities for Display and Validations

--- Data/Entities/Car.cs
+++ Data/Entities/Car.cs
@@ -1,11 +1,24 @@
-﻿namespace DVLD.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DVLD.Data.Entities
 {
     public class Car
     {
         public int Id { get; set; }
+
+
+        [Required]
+        [Display(Name = "Manufacturer")]
         public string Manu { get; set; }
+
+        [Required]
         public string Model { get; set; }
+
+        [Required]
         public string Color { get; set; }
+
+        [Required]
+        [Display(Name = "License Plate")]
         public string LicensePlate { get; set; }
 
         public Driver Driver { get; set; }
--- Data/Entities/Driver.cs
+++ Data/Entities/Driver.cs
@@ -1,5 +1,6 @@
 ﻿using DVLD.Enums;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DVLD.Data.Entities
@@ -7,10 +8,20 @@ namespace DVLD.Data.Entities
     public class Driver
     {
         public int Id { get; set; }
+
+
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+
         [NotMapped]
+        [Display(Name = "Full Name")]
         public string FullName
         {
             get
@@ -19,9 +30,22 @@ namespace DVLD.Data.Entities
             }
         }
 
+        [Required]
+        [Display(Name = "Date of birth")]
         public DateTime DateOfBirth { get; set; }
+
+
+        [Required]
+        [Display(Name = "License Number")]
         public string LicenseNumber { get; set; }
+
+
+        [Required]
+        [Display(Name = "License Type")]
         public LicenseType LicenseType { get; set; }
+
+
+        [Required]
         public Gender Gender { get; set; }
     }
 }
