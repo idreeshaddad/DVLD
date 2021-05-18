440fd1f93c6e4e2cd01525024d6ade5f4e94fabb
Idrees Haddad <gelnox@live.com>
2021-05-18 18:05:34 +0300

    - Added click image zoom jquery plugin - Smooth, soft, and silky scroll after showing all projects

--- Buildy/wwwroot/js/site.js
+++ Buildy/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+
+    $("#viewAllProjects").on("click", function () {
+
+        $(".more-project").slideDown(400, "swing", function () {
+            var distanceFromTopPlusProjects = $(document).height() + 580;
+            $('html, body').animate({ scrollTop: distanceFromTopPlusProjects }, 400);
+        });
+
+        $(this).hide();
+        $("#viewLessProjects").show();
+
+        
+    });
+
+    $("#viewLessProjects").on("click", function () {
+
+        $(".more-project").slideUp();
+
+        $(this).hide();
+        $("#viewAllProjects").show();
+    });
+});
