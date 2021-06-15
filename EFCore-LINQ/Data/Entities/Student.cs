4adbf3b8a3ccb875cdba3d41220c48094a2ceeff
Idrees Haddad <gelnox@live.com>
2021-06-15 19:14:15 +0300

    Linq: Any, Contains

--- EFCore-LINQ/Data/Entities/Student.cs
+++ EFCore-LINQ/Data/Entities/Student.cs
@@ -11,7 +11,10 @@ namespace EFCore_LINQ.Data.Entities
         }
 
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
         public DateTime DateOfBirth { get; set; }
         public List<Course> Courses { get; set; }
     }
