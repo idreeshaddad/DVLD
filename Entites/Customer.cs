da686fdc536130871a0707e9807c6cf9abbe8cf6
Blood Moon <hamze_2009@yahoo.com>
2021-06-17 23:27:59 +0300

    New Project (Cozmo) FirstPush automapper + Converted to ViewModel

---
+++ Entites/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entites
+{
+    public class Customer
+    {
+        public Customer()
+        {
+            Products = new List<Product>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
